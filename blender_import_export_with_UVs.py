ccf4d7d3d91a51c67e996bdccb1b413ae6222ca0
Ankur <handa.ankur@gmail.com>
2015-09-08 21:44:08 +0100

    minor edits
    Signed-off-by: Ankur <handa.ankur@gmail.com>

--- blender_import_export_with_UVs.py
+++ blender_import_export_with_UVs.py
@@ -1,13 +1,18 @@
+import sys
 import bpy,bmesh
 
-full_path_to_file = "night_stand_0026.obj"
+
+
+#print sys.argv[1]
+print ("First argument: %s" % str(sys.argv[5]))
+
+full_path_to_file = sys.argv[5] + '.obj' #"night_stand_0026.obj"
 bpy.ops.import_scene.obj(filepath=full_path_to_file, use_edges=True, use_smooth_groups=True, use_split_objects=True, use_split_groups=True,use_groups_as_vgroups=False, use_image_search=True, split_mode='ON',global_clamp_size=0, axis_forward='-Z', axis_up='Y')
 
 
 # Get the active mesh
-#me = bpy.context.active_object
-#bm = bmesh.from_edit_mesh(me)
-bpy.context.scene.objects.active = bpy.data.objects["night_stand_0026"]
+#bpy.context.scene.objects.active = bpy.data.objects["night_stand_0026"]
+bpy.context.scene.objects.active = bpy.data.objects[sys.argv[5]]
 
 bpy.ops.object.mode_set(mode='EDIT', toggle=False)
 
@@ -16,26 +21,10 @@ obj = bpy.context.active_object
 me = obj.data
 bm = bmesh.from_edit_mesh(me)
 
-#uv_layer = bm.loops.layers.uv.verify()
-#bm.faces.layers.tex.verify()# currently blender needs both layers.
-
-#adjust UVs
-#for f in bm.faces:
-#	for l in f.loops:
-#		luv = l[uv_layer]
-#		if luv.select: #apply the location of the vertex as a UV
-#			luv.uv = l.vert.co.xy
-
 bpy.ops.uv.smart_project()
 
-#image = bpy.data.images.new(name="test.jpg", width=800, height=600)
-
-#for uv_face in obj.data.uv_textures.active.data:
-#    uv_face.image = image
-
-#bpy.ops.object.bake_image()
-
-name = "night_stand_0026"
+#name = "night_stand_0026"
+name = sys.argv[5]
 
 mat = bpy.data.materials.new(name)
 
@@ -43,6 +32,7 @@ me.materials.append(mat)
 
 bmesh.update_edit_mesh(me)
 
-obj_out = "exporte_obj.obj"
+#obj_out = "export_copy.obj"
+obj_out = sys.argv[5] + "_copy.obj"
  
 bpy.ops.export_scene.obj(filepath=obj_out, axis_forward='-Z', axis_up='Y')
