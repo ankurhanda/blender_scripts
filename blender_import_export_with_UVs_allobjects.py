38d31660be0b2002f130f9cf12a52c272cc98a5e
Ankur Handa <handa.ankur@gmail.com>
2016-04-24 19:07:29 +0100

    updating mesh import export with UVs
    Signed-off-by: Ankur Handa <handa.ankur@gmail.com>

--- blender_import_export_with_UVs_allobjects.py
+++ blender_import_export_with_UVs_allobjects.py
@@ -1,38 +1,20 @@
 import sys
 import bpy,bmesh
 
-#print sys.argv[1]
 print ("First argument: %s" % str(sys.argv[5]))
 
 full_path_to_file = sys.argv[5] + '.obj' #"night_stand_0026.obj"
 bpy.ops.import_scene.obj(filepath=full_path_to_file, use_edges=True, use_smooth_groups=True, use_split_objects=True, use_split_groups=True,use_groups_as_vgroups=False, use_image_search=True, split_mode='ON',global_clamp_size=0, axis_forward='-Z', axis_up='Y')
 
 
-# Get the active mesh
-#bpy.context.scene.objects.active = bpy.data.objects["night_stand_0026"]
+for obj in bpy.data.objects:
+	print(obj.name)
+	if obj.name != "Camera" and obj.name != "Lamp":
+	        bpy.context.scene.objects.active = bpy.data.objects[obj.name]
+        	bpy.ops.object.mode_set(mode='EDIT', toggle=False)
+        	bpy.ops.uv.smart_project()
 
-D = bpy.data
 
-objects = [obj for obj in D.objects if obj.type == 'OBJECT']
-
-for obj in objects:
-
-        objname = obj.name
-
-        print(objname)
-
-        bpy.context.scene.objects.active = bpy.data.objects[objname]
-        bpy.ops.object.mode_set(mode='EDIT', toggle=False)
-
-        obj = bpy.context.active_object
-        me  = obj.data
-        bm  = bmesh.from_edit_mesh(me)
-
-        bpy.ops.uv.smart_project()
-
-        bmesh.update_edit_mesh(me)
-
-#obj_out = "export_copy.obj"
 obj_out = sys.argv[5] + "_UVexported.obj"
 
 bpy.ops.export_scene.obj(filepath=obj_out, axis_forward='-Z', axis_up='Y')
